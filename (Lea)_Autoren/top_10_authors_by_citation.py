eaa06365bc7bd3c6b1e57feb25ed847919a020f3
lea-mangelsen <lea.mangelsen.13@gmail.com>
2024-11-28 10:37:21 +0100

    add ICSE Dataset

--- (Lea)_Autoren/top_10_authors_by_citation.py
+++ (Lea)_Autoren/top_10_authors_by_citation.py
@@ -3,7 +3,7 @@ import altair as alt
 import os
 
 # Den Pfad zur CSV-Datei definieren (angenommen, sie liegt im 'data'-Ordner des Projekts)
-csv_file_path = os.path.join(os.getcwd(), 'data', '1994-2024.CSV')
+csv_file_path = os.path.join(os.getcwd(), 'data', 'Transactions_on_SE_1994-2024.csv')
 
 # CSV-Datei einlesen
 df = pd.read_csv(csv_file_path)
