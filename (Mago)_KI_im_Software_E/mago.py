c9743f3e71d92647c01dea84e7c74c8b2270b47f
maka3592 <maka3592@stud.hs-flensburg.de>
2024-12-06 18:59:52 +0100

    Update Mago

--- (Mago)_KI_im_Software_E/mago.py
+++ (Mago)_KI_im_Software_E/mago.py
@@ -7,3 +7,15 @@ csv_file_path = os.path.join(os.getcwd(), 'data', 'Dataset.csv')
 
 # CSV-Datei einlesen
 df = pd.read_csv(csv_file_path)
+
+# Zeige die ersten Zeilen des DataFrames an
+print(df.head())
+
+# Überblick über die Daten: Spaltennamen, Datentypen und Speicherplatz
+print(df.info())
+
+# Statistische Zusammenfassung der numerischen Daten
+print(df.describe())
+
+# Überprüfe, ob es fehlende Werte gibt
+print(df.isnull().sum())
